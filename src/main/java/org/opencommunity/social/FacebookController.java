fab1592a43f4724ad579b510484fd99a384d064d
massimiliano regis <massimiliano.regis@gmail.com>
2016-03-29 12:20:23 +0200

    Facebook operations

--- src/main/java/org/opencommunity/social/FacebookController.java
+++ src/main/java/org/opencommunity/social/FacebookController.java
@@ -2,11 +2,14 @@ package org.opencommunity.social;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.UrlResource;
+import org.springframework.social.facebook.api.Account;
 import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.Page;
 import org.springframework.social.facebook.api.PagedList;
 import org.springframework.social.facebook.api.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,11 +29,25 @@ public class FacebookController {
     	{    	
     	
     	return facebook.pageOperations().postPhoto("603116799778109",msg,new UrlResource(img));    	
+    	}    
+    @RequestMapping("/pages")
+    public @ResponseBody PagedList<Account> pages()
+    	{    	
+    	return facebook.pageOperations().getAccounts();
+    	}
+    @RequestMapping("/page/{id}")
+    public @ResponseBody Page page(@PathVariable String id)
+    	{    		
+    	return facebook.pageOperations().getPage(id);
+    	}
+    @RequestMapping("/page/{id}/picture")
+    public @ResponseBody Page pagePicture(@PathVariable String id)
+    	{    		
+    	return null;
     	}
     @RequestMapping("/friends")
     public @ResponseBody PagedList<Post> friends()
-    	{
-    	
+    	{    	
     	//return facebook.pageOperations().post(arg0, arg1)
     	return facebook.feedOperations().getHomeFeed();
     	}
